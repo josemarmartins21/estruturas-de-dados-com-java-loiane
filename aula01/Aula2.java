5f800cf37e9aa2b6d507dd18ffc154f31c5a62e8
Josemar <josemar21@outlook.pt>
2025-12-24 13:15:57 +0100

    Terminei as aula 1, 2 e 3

---
+++ aula01/Aula2.java
@@ -0,0 +1,8 @@
+public class Aula2 {
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(5);
+
+        int i=0;
+        i++;
+    }
+}
---
+++ aula01/Aula3.java
@@ -0,0 +1,18 @@
+public class Aula3 {
+    public static void main(String[] args) {
+        
+        Vetor vetor = new Vetor(3);
+        
+            
+        vetor.adiciona("Angola");
+        vetor.adiciona("Moçambique");
+        vetor.adiciona("Canada");
+
+        System.out.println(vetor.quantidade());
+
+        vetor.imprimir();
+
+        int i = 0; 
+        i++;
+    }
+}
